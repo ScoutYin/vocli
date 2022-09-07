106652194f530fd54887041c54635c00309566b7
ScoutYin <scoutyin.1900@gmail.com>
2022-09-07 21:34:20 +0800

    chore: update eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,6 @@
 module.exports = {
 	root: true,
 	env: {
-		browser: true,
 		node: true,
 		es2021: true,
 	},
@@ -17,12 +16,4 @@ module.exports = {
 		'prettier/prettier': 'warn',
 		'@typescript-eslint/no-unused-vars': 'error',
 	},
-	overrides: [
-		{
-			files: ['build/*.js'],
-			rules: {
-				'@typescript-eslint/no-var-requires': 0,
-			},
-		},
-	],
 };
