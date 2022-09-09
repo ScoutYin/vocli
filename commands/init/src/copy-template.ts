1f50a9f0eceea72c76a051ca074430e3449e98a9
ScoutYin <scoutyin.1900@gmail.com>
2022-09-10 02:13:51 +0800

    refactor: generate package.json

--- commands/init/src/copy-template.ts
+++ commands/init/src/copy-template.ts
@@ -1,27 +1,12 @@
 import fs from 'fs-extra';
 import { resolve } from 'path';
-// import { createRequire } from 'node:module';
 import type { Answers } from './index';
 
 export const copyTemplate = async (
 	projectDir: string,
 	{ projectName }: Pick<Answers, 'projectName'>
 ) => {
-	// console.log(process.cwd());
-
-	// const require = createRequire(process.cwd());
-
-	// console.log(require.resolve.paths('@vocli/init'));
-	// const templatePackageDir = require.resolve('@vocli/init');
-
-	// console.log(templatePackageDir, '--templatePackageDir--');
-
-	// if (!templatePackageDir) return;
-
-	// const templateDir = resolve(templatePackageDir, 'template');
 	const templateDir = resolve(import.meta.url, './template');
-	console.log(import.meta.url, 'import.meta.url');
-	console.log(templateDir, '---templateDir--');
 
 	await fs.copy(templateDir, projectDir, {
 		filter: (src, dest) => {
@@ -30,9 +15,13 @@ export const copyTemplate = async (
 	});
 
 	// 修改项目 package.json 中的 name 字段为用户输入的项目名
-	const packageJsonPath = resolve(projectDir, 'package.json');
+	let packageJsonPath = resolve(projectDir, 'package.json.txt');
 	const packageJsonTpl = await fs.readFile(packageJsonPath, 'utf-8');
 	const packageJson = JSON.parse(packageJsonTpl);
 	packageJson.name = projectName;
+
+	fs.remove(packageJsonPath);
+
+	packageJsonPath = resolve(projectDir, 'package.json');
 	await fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
 };
