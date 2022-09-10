bbb00efbcb64875a3f846ec46325d6810b2d9273
ScoutYin <scoutyin.1900@gmail.com>
2022-09-10 19:31:53 +0800

    build: fix order of building dependence

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # vocli
+
+## TODO
+
+- build 顺序
+- build types
+- add command
+- publish
--- scripts/build.ts
+++ scripts/build.ts
@@ -9,18 +9,23 @@ interface BuildConfig {
 	dir: string;
 }
 
-const packageBuildConfigs: BuildConfig[] = [
+const sharedBuildConfig: BuildConfig = {
+	dir: 'packages/shared',
+};
+
+const commandBuildConfigs: BuildConfig[] = [
 	{
 		dir: 'commands/init',
 	},
-	{
-		dir: 'packages/shared',
-	},
-	{
-		dir: 'packages/vocli',
-	},
 ];
 
+const vocliBuildConfig: BuildConfig = {
+	dir: 'packages/vocli',
+};
+
+/**
+ * Move `commands/init/src/template` to packages/vocli/dist/template
+ */
 const copyTemplate = async () => {
 	const vocliTemplatePath = resolve('packages/vocli/dist/template');
 	if (fs.pathExistsSync(vocliTemplatePath)) {
@@ -36,47 +41,58 @@ const copyTemplate = async () => {
 	});
 };
 
+const buildPackage = (packageConfig: BuildConfig) => {
+	const buildCommonOptions: BuildOptions = {
+		entryPoints: [`${packageConfig.dir}/src/index.ts`],
+		bundle: true,
+		platform: 'node',
+		target: 'node14.16.0',
+		outdir: `${packageConfig.dir}/dist`,
+	};
+
+	return [
+		// for ESM
+		esbuild.build({
+			...buildCommonOptions,
+			format: 'esm',
+			tsconfig: 'tsconfig.esm.json',
+			outExtension: {
+				'.js': '.mjs',
+			},
+		}),
+		// for CommonJS
+		esbuild.build({
+			...buildCommonOptions,
+			format: 'cjs',
+			tsconfig: 'tsconfig.cjs.json',
+			outExtension: {
+				'.js': '.cjs',
+			},
+			// `import.meta.url` is not supported in CommonJS，replace whit `__dirname`
+			define: {
+				'import.meta.url': '__dirname',
+			},
+		}),
+	];
+};
+
 const build = async () => {
 	const buildTasks: Promise<BuildResult>[] = [];
 
-	packageBuildConfigs.forEach((pkg) => {
-		const buildCommonOptions: BuildOptions = {
-			entryPoints: [`${pkg.dir}/src/index.ts`],
-			bundle: true,
-			platform: 'node',
-			target: 'node14.16.0',
-			outdir: `${pkg.dir}/dist`,
-		};
-
-		buildTasks.push(
-			// for ESM
-			esbuild.build({
-				...buildCommonOptions,
-				format: 'esm',
-				tsconfig: 'tsconfig.esm.json',
-				outExtension: {
-					'.js': '.mjs',
-				},
-			}),
-			// for CommonJS
-			esbuild.build({
-				...buildCommonOptions,
-				format: 'cjs',
-				tsconfig: 'tsconfig.cjs.json',
-				outExtension: {
-					'.js': '.cjs',
-				},
-				// `import.meta.url` is not supported in CommonJS，replace whit `__dirname`
-				define: {
-					'import.meta.url': '__dirname',
-				},
-			})
-		);
+	commandBuildConfigs.forEach((packageConfig) => {
+		buildTasks.push(...buildPackage(packageConfig));
 	});
 
 	// TODO: build types.
 
+	// build shared package, as a dependence, so it needs build first.
+	await Promise.all(buildPackage(sharedBuildConfig));
+
+	// build commands
 	await Promise.all([...buildTasks, copyTemplate()]);
+
+	// build vocli package, it depends on all packages, so build it at last.
+	await Promise.all(buildPackage(vocliBuildConfig));
 
 	console.log(chalk.green(`  Build succeeded!`));
 	console.log();
